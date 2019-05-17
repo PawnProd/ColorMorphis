dbba5eb962231cdf80c9482aa1ba63fa1c7e4b22
Pawn <nathan.prospert@pawnproduction.fr>
2019-05-17 22:26:11 +0200

    Init project + character movement with isometric view

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 0;
+
+    public void Move(Vector3 direction, float deltaTime)
+    {
+        Vector3 movement = speed * deltaTime * direction.normalized;
+
+        transform.Translate(movement);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        
+        Vector3 direction = new Vector3(h,0,v);
+        
+        if(direction != Vector3.zero)
+            _controller.Move(direction, Time.deltaTime);
+        
+    }
+}
